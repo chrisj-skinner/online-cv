2ee85d7010aab7d1d573f8be248d24f45da318c2
Chris Skinner <c.skinner@blueyonder.co.uk>
2016-01-01 17:23:17 +0100

    Update template string | add ajax call | set fadeIn timers for amended dom elements

--- js/theme.js
+++ js/theme.js
@@ -35,12 +35,19 @@
 
         let exports = {},
         // Intro to portfolios
-        teaserHTML = $('.portfolio-wrap');
+        teaserHTML = $('.portfolio-wrap'),
+        showMore = $('.show-more');
 
         exports.init = function(count, domElement = teaserHTML){
 
             // Add ajax-loading
             $(domElement).addClass('loading');
+
+            // Call ajaxCall func
+            exports.ajaxCall(count, domElement);
+        };  
+
+        exports.ajaxCall = function(count, domElement){
 
             let teaserContent = '',
                 portfolioContent = '',
@@ -50,29 +57,20 @@
                 portfolioHTML = $('.portfolio-modal').last(),
                 // Current portfolios
                 current = $('.portfolio-item').length,
-                showMore = $('.show-more').css('display', 'block');
                 // Set the limit
                 limit = current + count;
     
             // Ajax call
-            jqxhr = $.getJSON( "js/datda.json", function() {
+            jqxhr = $.getJSON( "js/data.json", function() {
 
             })
-              .done(function(data) {
-                portfolios = data;
-              })
-              .fail(function(data, error, textStatus) {
-                teaserHTML.append('Error</br >');
-              });
-             
-            // Set a completion function for ajax request
-            jqxhr.complete(function() {
-                
+            .done(function(data) {
+
                 // Begin loop
                 for(let i = current; i < limit; i++){
 
                     // Check if current portfolio exists
-                    if (portfolios[i] === undefined) {
+                    if (data[i] === undefined) {
 
                         // Change show-more button -> no-more
                         showMore.html('No more').attr('disabled', 'true');
@@ -89,7 +87,7 @@
                                     <i class="fa fa-search-plus fa-3x"></i>
                                 </div>
                             </div>
-                            <img src="img/portfolio/cabin.png" class="img-responsive" alt="">
+                            <img src="${data[i].img.sizes.thumbnail}" class="img-responsive" alt="">
                         </a>
                     </div>`;
 
@@ -106,21 +104,21 @@
                                     <div class="row">
                                         <div class="col-lg-8 col-lg-offset-2">
                                         <div class="modal-body">
-                                        <h2>${portfolios[i].title}</h2>
+                                        <h2>${data[i].title}</h2>
                                         <hr class="star-primary">
-                                        <img src="img/portfolio/game.png" class="img-responsive img-centered" alt="">
-                                        <p>${portfolios[i].description}</p>
+                                        <img src="${data[i].img.sizes.full}" class="img-responsive img-centered" alt="">
+                                        <p>${data[i].description}</p>
                                         <ul class="list-inline item-details">
                                             <li>Client:
-                                            <strong><a href="${portfolios[i].client.url}">${portfolios[i].client.name}</a>
+                                            <strong><a href="${data[i].client.url}">${data[i].client.name}</a>
                                             </strong>
                                             </li>
                                             <li>Date:
-                                            <strong><a href="http://startbootstrap.com">${portfolios[i].date}</a>
+                                            <strong><a href="#">${data[i].date}</a>
                                             </strong>
                                             </li>
                                             <li>Service:
-                                            <strong><a href="http://startbootstrap.com">${portfolios[i].service}</a>
+                                            <strong><a href="#">${data[i].service}</a>
                                             </strong>
                                             </li>
                                         </ul>
@@ -134,16 +132,28 @@
 
                 } // end for loop
 
-                // Add teaserContent to html / remove class
-                teaserHTML.append(teaserContent);
+                // Add teaserContent to html with fadeIn
+                teaserHTML.append($(teaserContent).fadeIn(1500));
+
+                // Fade in showMore button
+                showMore.fadeIn(1250);
 
                 // Add portfolioContent to html
                 portfolioHTML.after(portfolioContent);
 
-                // Remove loading class
-                $(domElement).removeClass('loading');
+            })
+            .fail(function() {
+                // Display error
+            })
+            .always(function(){
+                // Remove loading
+                exports.removeLoading(domElement);
+            });
 
-            }); // end .complete
+            // Declare removeLoading func
+            exports.removeLoading = function(element){
+                $(element).removeClass('loading');
+            };
         };
 
         return exports;
@@ -153,10 +163,9 @@
     // Run func
     displayPortfolios.init(6);
 
-    // Ajax show more
+    // Show more button
     $('.show-more').on('click', function(event){
         displayPortfolios.init(3, $(event.currentTarget));
-
     });
 
 
