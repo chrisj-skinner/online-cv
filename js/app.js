a695cd00b6295863ee262160d23b67751ea6f0ec
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-02-05 11:37:50 +0000

    :fire:

--- js/app.js
+++
@@ -1,8 +0,0 @@
-// Dependencies
-var jQuery = require('jquery');
-window.$ = window.jQuery = jQuery;
-
-require('../vendor/bootstrap/dist/js/npm.js');
-require('jquery.easing');
-require('./modules/theme.js');
-require('./modules/ajaxCall.js');
